d688362e771d06246a61dc059505be462e2a8252
awaitingsignal <awaitingsignal@protonmail.com>
2019-03-05 18:07:35 -0500

    toast and finish working

--- app/src/main/java/com/ravebiz/day003contactcard/MainActivity.java
+++ app/src/main/java/com/ravebiz/day003contactcard/MainActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(MainActivity.this );
         if (getIntent().hasExtra("NAME")) {
             TextView t = findViewById(R.id.myNameTextView);
             t.setText(getIntent().getStringExtra("NAME"));
@@ -65,8 +70,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(homeIntent);
     }
 
-//    @OnClick(R.id.buttonlogout)
-//    public void clickSubmit() {
-//        Toast.makeText(this, "Mmmm.... toast", Toast.LENGTH_SHORT).show();
-//    }
+    @OnClick(R.id.buttonlogout)
+    public void clickSubmit() {
+        Toast.makeText(this, "Finishing...", Toast.LENGTH_SHORT).show();
+        finish();
+    }
 }
